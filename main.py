ced6ba83018ca753f1504155a921411357687df6
Kolobkov <artemk2803@bk.ru>
2022-10-09 20:05:13 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Линкор - 1 (4)
+# Крейсер - 2 (3)
+# Эсминец - 3 (2)
+
+# структура списка:
+# x, y, есть ли корабль, есть ли попадание
+
+# field
+field_size = 8
+
+field1 = []
+field2 = []
+# field
+
+def create_field(field_size):
+    pass
+
+
+def create_field_test():
+    pass
